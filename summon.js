2cd33f52c863aa16da7a504612cc87799e6194fe
Gin-cell <assasin1290@gmail.com>
2021-08-28 21:23:59 +0300

    1

--- summon.js
+++ summon.js
@@ -1,11 +1,30 @@
 const servant_url = "https://api.atlasacademy.io/export/NA/nice_servant.json";
 const ce_url = "https://api.atlasacademy.io/export/NA/nice_equip.json";
 const theButton = document.getElementById("myButton");
+var locked = false;
+
+async function fetchWithTimeout(resource, options = {}) {
+  const { timeout = 8000 } = options;
+  
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+
+  const response = await fetch(resource, {
+    ...options,
+    signal: controller.signal  
+  });
+  clearTimeout(id);
+
+  return response;
+}
 
 async function rollgacha() {
-  const response_srv = await fetch(servant_url);
+  if (!locked)
+  {
+    locked = true;
+  const response_srv = await fetchWithTimeout(servant_url);
   const servants_raw = await response_srv.json();
-  const response_ce = await fetch(ce_url);
+  const response_ce = await fetchWithTimeout(ce_url);
   const crafting_essences_raw = await response_ce.json();
   const crafting_essences = {
     "3": crafting_essences_raw.filter(essence => essence.rarity == 3),
@@ -39,7 +58,7 @@ async function generateRandomRareCard(essences, servants, rarity){
     var colors = {
     "3": "#AD8A56",
     "4": "#D7D7D7",
-    "5": "#FFD933"
+    "5": "#AF9500"
     }
     if (rarity.length > 1){
         cards = essences;
@@ -49,22 +68,26 @@ async function generateRandomRareCard(essences, servants, rarity){
     var cards_pool = cards[rarity[0]]
     var card = cards_pool[Math.floor(Math.random()*cards_pool.length)];
     var img_url = '';
-    var result = '<div style="background-color:' + colors[rarity[0]] + ';">' + card.name;
+    var result = '<div class="rollContainer" style="background-color:' + colors[rarity[0]] + ';">' + card.name;
     if (card.className != null){
         img_url = card.extraAssets.faces.ascension[2];
         result += ', ' + card.className;
     } else {
         img_url = Object.values(card.extraAssets.faces.equip)[0];
     }
-    const response_img = await fetch(img_url);
-    var blob = await response_img.blob();
-    result += '<br><img src="' + URL.createObjectURL(blob) + '"></div>';
+    try{
+        const response_img = await fetchWithTimeout(img_url);
+        var blob = await response_img.blob();
+        result += '<br><img src="' + URL.createObjectURL(blob) + '"></div>';
+    }
+    catch(e){
+        result += '<br><img src="' + img_url + '"></div>';
+    }
     return result;
   }
 
   async function rollCards(essences, servants, weighted_rarity, len){
     result = (await Promise.all(Array.from({length: len}, async () => await generateRandomRareCard(essences, servants, weightedChoice(weighted_rarity))))).join('');
-    theButton.classList.remove("button--loading");
     return result;
 }
 
@@ -78,7 +101,10 @@ async function generateRandomRareCard(essences, servants, rarity){
 }
 
   var result = await rollCards(crafting_essences, servants, weighted_rarity, 11);
+  theButton.classList.remove("button--loading");
   document.getElementById("Bros, this code...").innerHTML = result;
+  locked = false;
+}
 }
 document.getElementById('myButton').addEventListener('click', function(rollgacha) {
   rollgacha.preventDefault();
